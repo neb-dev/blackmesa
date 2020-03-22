30a2da2b13f497b975c9b0c1f53a824d9b6d768f
neb <10528790@my.uvu.edu>
2020-03-21 23:19:54 -0600

    added inline form to nav, added jumotron

--- index.php
+++ index.php
@@ -13,32 +13,36 @@
 <body>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-      <a class="navbar-brand" href="#">Black Mesa Research Facility</a>
+      <a class="navbar-brand" href="http://blackmesa/">Black Mesa Research Facility</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
+
+      <form class="form-inline">
+        <div class="form-group mb-2 mr-sm-2">
+          <label class="sr-only" for="inlineFormInputGroupUsername">Username</label>
+          <input type="text" class="form-control" id="inlineFormInputGroupUsername" placeholder="Username">
+        </div>
+
+        <div class="form-group mb-2 mr-sm-2">
+          <label class="sr-only" for="exampleInputPassword">Password</label>
+          <input type="password" class="form-control" id="exampleInputPassword" placeholder="Password">
+        </div>
+
+        <button type="submit" class="btn btn-primary mb-2">Sign In</button>
+      </form>
     </div>
   </nav>
 
-  <div class="container-fluid bg-dark h-100">
-    <div class="row justify-content-center">
-      <div class="col-4">
-        <form>
-          <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  <div class="container h-100">
+    <div class="row">
+      <div class="col-12">
+        <div class="jumbotron jumbotron-fluid">
+          <div class="container">
+            <h1 class="display-4">Fluid jumbotron</h1>
+            <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
           </div>
-          <div class="form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-          </div>
-          <div class="form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-          </div>
-          <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
+        </div>
       </div>
     </div>
   </div>
