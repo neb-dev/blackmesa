ccfb3a15736d17b3b9c08dbf19605c126c4ff332
neb <jn.web.developer@gmail.com>
2020-05-06 23:08:03 -0600

    html, php, and css code updates

--- signin.php
+++ signin.php
@@ -1,7 +1,7 @@
 <?php
   $server = "localhost";
-  $username = "root";
-  $password = "";
+  $username = "blackmesa";
+  $password = "pottop77";
 
   $conn = new mysqli($server, $username, $password);
   if($conn -> connect_error) {
