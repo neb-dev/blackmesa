da794be8a71f808d4f2389df2cc6f0cd55724309
neb <10528790@my.uvu.edu>
2020-05-21 18:16:17 -0600

    added comments to post.php

--- testing/post.php
+++ testing/post.php
@@ -9,22 +9,30 @@ $actionURL = "http://testing-ground.scraping.pro/login?mode=login";
 
 $ch = curl_init();
 
+// URL where POST request is being sent
 curl_setopt($ch, CURLOPT_URL, $actionURL);
 
+// set cURL POST request to true
 curl_setopt($ch, CURLOPT_POST, true);
 
+// sends username and password
 curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
 
+// stores cookie authentication data
 curl_setopt($ch, CURLOPT_COOKIEJAR, 'cookie.txt');
 
+// returns the output
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
+// refers cURL to the form URL
 curl_setopt($ch, CURLOPT_REFERER, $url);
 
+// allows redirects
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 
 echo curl_exec($ch);
 
+// error checking
 if(curl_errno($ch)){
     throw new Exception(curl_error($ch));
 }
