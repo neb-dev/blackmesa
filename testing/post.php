a03d1d1139c0617dd43b1db1aa42d3e8199482fa
neb <10528790@my.uvu.edu>
2020-05-21 18:09:20 -0600

    working cURL post request

--- testing/post.php
+++ testing/post.php
@@ -1,24 +1,33 @@
 <?php
-
 $data = array(
-    "username" => "neb",
-    "password" => "password",
-    "form" => "submit"
+    "usr" => "admin",
+    "pwd" => "12345"
 );
 
-$ch = curl_init("signin.php");
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+$url = "http://testing-ground.scraping.pro/login";
+$actionURL = "http://testing-ground.scraping.pro/login?mode=login";
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, $actionURL);
+
 curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-curl_setopt($ch, CURLOPT_COOKIEJAR, "cookie.txt");
+
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+
+curl_setopt($ch, CURLOPT_COOKIEJAR, 'cookie.txt');
+
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-$res = curl_exec($ch);
+curl_setopt($ch, CURLOPT_REFERER, $url);
 
-var_dump($res);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+echo curl_exec($ch);
+
+if(curl_errno($ch)){
+    throw new Exception(curl_error($ch));
+}
 
 curl_close($ch);
-
-echo $res
-
 ?>
